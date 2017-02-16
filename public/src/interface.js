1daf491e2409a299726b01ac31931ae88d2ab1bc
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-02-16 11:55:13 +0000

    Guardian JSON in function

--- public/src/interface.js
+++ public/src/interface.js
@@ -1,15 +1,20 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk-news?show-fields=body&show-blocks=all');
-xhr.onload = function() {
-    if (xhr.status === 200) {
-      for (var i=0; i<=5; i++){
-        guardianObject = JSON.parse(xhr.responseText).response.results[i];
-        document.write("<b>"+guardianObject.webTitle+"</b>");
-        document.write(guardianObject.fields.body.substring(0,100)+"...<br><br>");
+
+function readGuardian(articles=5){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk-news?show-fields=body&show-blocks=all');
+  xhr.onload = function() {
+      if (xhr.status === 200) {
+        for (var i=0; i<=articles; i++){
+          guardianObject = JSON.parse(xhr.responseText).response.results[i];
+          document.write("<b>"+guardianObject.webTitle+"</b>");
+          document.write(guardianObject.fields.body.substring(0,100)+"...<br><br>");
+        }
       }
-    }
-    else {
-        alert('Request failed.  Returned status of ' + xhr.status);
-    }
-};
-xhr.send();
+      else {
+          alert('Request failed.  Returned status of ' + xhr.status);
+      }
+  };
+  xhr.send();
+}
+
+readGuardian(5);
