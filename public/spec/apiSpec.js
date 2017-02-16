39a2badc3debbc7c5004606ed513a7b9ef7d77e7
James Turner <mail.jt@gmail.com>
2017-02-16 16:22:08 +0000

    stuff

--- public/spec/apiSpec.js
+++ public/spec/apiSpec.js
@@ -1,14 +1,23 @@
 "use strict";
 
 describe("getURL", function(){
+
+  var url = 'http://localhost:8080/sample_data.json'
   it("is able to connect to the server", function(){
-    getURL(function(){
+    getURL(url, function(){
       expect(this.status).toEqual(200);
     });
   })
   it("contains 'UK News'", function() {
-    getURL(function(){
+    getURL(url, function(){
       expect(this.responseText).toContain("UK news")
     });
   })
+})
+
+guardianApi("this",function(){
+  JSON.parse(this.responseText).response.results[i];
+  console.log("whatever")
+  console.log(this.responseText)
+
 })
--- public/src/api.js
+++ public/src/api.js
@@ -1,14 +1,32 @@
+"use strict";
 (function(exports){
 
-  getURL = function(loadedFunction){
-    xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:8080/sample_data.json');
-    xhr.onload = loadedFunction;
+  function getURL(url, loadedFunction, parser){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = loadedFunction
     xhr.send();
     return xhr;
+  };
 
+  function guardianApi(someotherstuff,loadedFunction,parser){
+    var shellURL = "http://news-summary-api.herokuapp.com/guardian"
+    var guardianURL = "?apiRequestUrl=http://content.guardianapis.com"
+    var generatedURL = shellURL + guardianURL
+    function guardianParser(){}
+    return getURL(generatedURL, guardianParser)
   };
 
   exports.getURL = getURL;
+  exports.guardianApi = guardianApi;
 
 })(this);
+
+
+
+
+
+
+
+
+JSON.parse(xhr.responseText).response.results[i];
