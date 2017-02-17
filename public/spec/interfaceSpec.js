cecdc54782f895da893a551f2a6b1e83892367f4
Benjamin Carson <benjohncarson@gmail.com>
2017-02-17 11:50:07 +0000

    started refactor

--- public/spec/interfaceSpec.js
+++ public/spec/interfaceSpec.js
@@ -1,9 +1,21 @@
 'use strict';
 
 describe("Guardian", function() {
-  it("Reads Guardian JSON object", function(){
-      var newsId = "<li class=note id=news-test>"
-      var title = "test title";
+  
+  beforeEach(function() {
+    var url = testUrl();
+    guardianApi(url);
+  });
+  
+  // Pending, needs a spy
+  // it("iterates over headlineArray and passes newsId and title to writeHeadline", function() {
+  //     updateHeadlines();
+      
+  // })
+  
+  it("writes id and title to contentArray", function(){
+    var newsId = "<li class=note id=news-test>";
+    var title = "test title";
     expect(writeHeadline(newsId, title)).toContain("test title");
   });
-})
+});
--- public/src/interface.js
+++ public/src/interface.js
@@ -1,53 +1,61 @@
-var headlineArray = [];
-var contentArray = [];
+"use strict";
 
-function getHeadlines(){
-  headlineArray = guardianApi();
-}
 
-function updateHeadlines(){
-  for (var i=0; i<headlineArray.length; i++){
-    {
-      var newsId = "<li class=note id=news-" + i + ">";
-      var title = headlineArray[i].webTitle;
-      writeHeadline(newsId, title);
+(function(exports) {
+  
+  
+
+  var headlineArray = [];
+  var contentArray = [];
+  
+  function getHeadlines(){
+    headlineArray = guardianApi();
+  }
+  
+  function updateHeadlines(){
+    for (var i=0; i<headlineArray.length; i++){
+      {
+        var newsId = "<li class=note id=news-" + i + ">";
+        var title = headlineArray[i].webTitle;
+        writeHeadline(newsId, title);
+      }
     }
   }
-}
+  
+  function writeHeadline(newsId, title){
+    content = newsId + title + "</li>"
+    contentArray.push(content);
+    return content;
+  }
+  
+  
+  function createHeadlines(){
+      for (var i=0; i<contentArray.length; i++){
+        document.getElementById('headlines').innerHTML += contentArray[i];
+      }
+   }
+  
+  // function readGuardian(articles=5){
+  //
+  //   var xhr = new XMLHttpRequest();
+  //   xhr.open('GET', 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk-news?show-fields=body&show-blocks=all');
+  //   xhr.onload = function() {
+  //       if (xhr.status === 200) {
+  //         for (var i=0; i<=articles; i++){
+  //           guardianObject = JSON.parse(xhr.responseText).response.results[i];
+  //           headlineArray.push(guardianObject);
+  //         }
+  //         updateHeadlines();
+  //       }
+  //       else {
+  //           alert('Request failed.  Returned status of ' + xhr.status);
+  //       }
+  //   };
+  //   xhr.send();
+  // }
+  
+  getHeadlines();
+  updateHeadlines();
+  createHeadlines();
 
-function writeHeadline(newsId, title){
-  content = newsId + title + "</li>"
-  contentArray.push(content);
-  console.log(contentArray);
-  return content;
-}
-
-
-function createHeadlines(){
-    for (var i=0; i<contentArray.length; i++){
-      document.getElementById('headlines').innerHTML += contentArray[i];
-    }
- }
-
-// function readGuardian(articles=5){
-//
-//   var xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk-news?show-fields=body&show-blocks=all');
-//   xhr.onload = function() {
-//       if (xhr.status === 200) {
-//         for (var i=0; i<=articles; i++){
-//           guardianObject = JSON.parse(xhr.responseText).response.results[i];
-//           headlineArray.push(guardianObject);
-//         }
-//         updateHeadlines();
-//       }
-//       else {
-//           alert('Request failed.  Returned status of ' + xhr.status);
-//       }
-//   };
-//   xhr.send();
-// }
-
-getHeadlines();
-updateHeadlines();
-createHeadlines();
+})(this);
