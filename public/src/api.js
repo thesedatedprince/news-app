1c38f5fa91ab03423033dd080124b65e8f40f28f
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-17 14:45:51 +0000

    tweaked one test to pass

--- public/src/api.js
+++ public/src/api.js
@@ -12,22 +12,21 @@
     }
 
     function results(object) {
-        var objArray = objArray || [object];
-        if (object){
-          objArray.push(object)
+        if (object) {
+            objArray.push(object)
+        } else {
+            return objArray
         }
-        else{
-        return objArray
-      }
 
     }
 
-    function guardianApi(url = "https://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk-news?show-fields=body&show-blocks=all", articles=5) {
+    function guardianApi(url = "https://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/uk-news?show-fields=body&show-blocks=all", articles = 5) {
         // var shellURL = "http://news-summary-api.herokuapp.com/guardian";
         // var guardianURL = "?apiRequestUrl=http://content.guardianapis.com";
         // var generatedURL = shellURL + guardianURL
         var generatedURL = "http://localhost:8080/sample_data.json";
         getURL(url, guardianParser);
+
         function guardianParser() {
             for (var i = 0; i <= articles; i++) {
                 results(JSON.parse(this.responseText).response.results[i]);
@@ -35,16 +34,6 @@
         }
         return results();
     }
-
-    //////////// example - this will be in the interface file ////////////
-    // function updateHeadlines() {
-    //     var stories = guardianApi("http://localhost:8080/sample_data.json");
-    //     for (var i = 0; i < stories.length; i++) {
-    //         var newsId = "<li class=note id=news-" + i + ">";
-    //         var title = stories[i].webTitle;
-    //         writeHeadline(newsId, title)
-    //     }
-    // }
 
     exports.getURL = getURL;
     exports.results = results;
